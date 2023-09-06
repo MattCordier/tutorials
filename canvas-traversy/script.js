7242098c4759da26fef14dc296f1e0c30a320211
Matt Cordier <mcordier24@gmail.com>
2023-09-06 06:46:10 -1000

    bouncing ball animation

--- canvas-traversy/script.js
+++ canvas-traversy/script.js
@@ -56,28 +56,76 @@ console.log(ctx)
 // ctx.fillStyle = 'teal'
 // ctx.fill();
 
-// Arcs
-const centerX = canvas.width / 2;
-const centerY = canvas.height / 2;
+// // Arcs
+// const centerX = canvas.width / 2;
+// const centerY = canvas.height / 2;
 
-// Draw head
-ctx.arc(centerX, centerY, 200, 0, Math.PI * 2);
+// // Draw head
+// ctx.arc(centerX, centerY, 200, 0, Math.PI * 2);
 
-// Move to mouth
-ctx.moveTo(centerX + 100, centerY)
+// // Move to mouth
+// ctx.moveTo(centerX + 100, centerY)
 
-// Draw mouth
-ctx.arc(centerX, centerY, 100, 0, Math.PI, false);
+// // Draw mouth
+// ctx.arc(centerX, centerY, 100, 0, Math.PI, false);
 
-// Move to left eye
-ctx.moveTo(centerX - 60, centerY - 80)
+// // Move to left eye
+// ctx.moveTo(centerX - 60, centerY - 80)
 
-// Draw left eye
-ctx.arc(centerX - 80, centerY - 80, 20, 0, Math.PI * 2, false);
+// // Draw left eye
+// ctx.arc(centerX - 80, centerY - 80, 20, 0, Math.PI * 2, false);
 
-// Move to right eye
-ctx.moveTo(centerX + 100, centerY - 80)
+// // Move to right eye
+// ctx.moveTo(centerX + 100, centerY - 80)
 
-// Draw right eye
-ctx.arc(centerX + 80, centerY - 80, 20, 0, Math.PI * 2, false);
-ctx.stroke();
+// // Draw right eye
+// ctx.arc(centerX + 80, centerY - 80, 20, 0, Math.PI * 2, false);
+// ctx.stroke();
+
+// // ToDo: Curves
+// // Quadratic curves have one control point
+// // quadraticCurveTo(cp1x, cp1y, x, y)
+
+// // Bezier curves have two control points
+// // bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y)  
+
+// Animation 1
+
+// create an object for each element
+
+const circle = {
+    x: 200,
+    y: 200,
+    size: 30,
+    dx: 5, // increment of movement on x-axis
+    dy: 4 // increment of movement on y-axis
+};
+
+function drawCircle() {
+    ctx.beginPath();
+    ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
+    ctx.fillStyle = 'purple';
+    ctx.fill();
+}
+
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    drawCircle();
+
+    // change position
+    circle.x += circle.dx;
+    circle.y += circle.dy;
+
+    // detect side walls
+    if (circle.x + circle.size > canvas.width || circle.x - circle.size < 0) {
+        circle.dx *= -1; //multiplies circle.dx by -1
+    }
+
+    // detect top and bottom wall
+    if (circle.y + circle.size > canvas.height || circle.y - circle.size < 0) {
+        circle.dy *= -1; //multiplies circle.dx by -1
+    }
+    requestAnimationFrame(update);
+}
+
+update();
