8f46ba1831e615c1b8514ce89ca015f955af9fff
Matt Cordier <mcordier24@gmail.com>
2023-09-07 07:50:12 -1000

    particle system

--- canvas-franks-lab/script.js
+++ canvas-franks-lab/script.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const particlesArray = [];
 
 window.addEventListener('resize', function(){
     canvas.width = window.innerWidth;
@@ -29,30 +30,61 @@ canvas.addEventListener('mousemove', function(e){
     mouse.y = e.y;
 })
 
-function drawEyeBall() {
-    ctx.beginPath();
-    ctx.fillStyle = 'white';
-    ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
-    ctx.fill();
+class Particle {
+    constructor() {
+        // this.x = mouse.x;
+        // this.y = mouse.y;
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 5 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+    }
 
-    ctx.beginPath();
-    ctx.strokeStyle = 'red';
-    ctx.lineWidth = 5;
-    ctx.arc(mouse.x, mouse.y, 30, 0, Math.PI * 2);
-    ctx.stroke();
+    update(){
+        this.x += this.speedX;
+        this.y += this.speedY
+    }
 
-    ctx.beginPath();
-    ctx.fillStyle = 'black';
-    ctx.arc(mouse.x, mouse.y, 27, 0, Math.PI * 2);
-    ctx.fill();
+    draw(){
+        ctx.beginPath();
+        ctx.fillStyle = 'white';
+        ctx.arc(this.x, this.y, 20, 0, Math.PI * 2);
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.strokeStyle = 'red';
+        ctx.lineWidth = 5;
+        ctx.arc(this.x, this.y, 10, 0, Math.PI * 2);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.fillStyle = 'black';
+        ctx.arc(this.x, this.y, 7, 0, Math.PI * 2);
+        ctx.fill();
+    }
 }
+
+function handleParticles() {
+    for (let i = 0; i < particlesArray.length; i++){
+        particlesArray[i].update();
+        particlesArray[i].draw();
+    }
+}
+
+function init() {
+    for (let i = 0; i < 100; i++){
+        particlesArray.push(new Particle())
+    }
+}
+console.log(particlesArray)
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawEyeBall();
+    handleParticles();
     requestAnimationFrame(animate);
 }
-
+init();
 animate();
 
 
