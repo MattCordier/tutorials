e114645279ca82fd183eda1b3385c9c2d9834b5c
Matt Cordier <mcordier24@gmail.com>
2023-09-11 08:51:08 -1000

    add constellations effect

--- canvas-franks-lab/script.js
+++ canvas-franks-lab/script.js
@@ -41,7 +41,7 @@ class Particle {
     constructor() {
         this.x = mouse.x;
         this.y = mouse.y;
-        this.size = Math.random() * 16 + 1;
+        this.size = Math.random() * 15 + 1;
         this.speedX = Math.random() * 3 - 1.5;
         this.speedY = Math.random() * 3 - 1.5;
         this.color = 'hsl('+ hue +', 100%, 50%)';
@@ -56,8 +56,8 @@ class Particle {
     }
 
     draw(){
-        ctx.beginPath();
         ctx.fillStyle = this.color;
+        ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.fill();      
     }
@@ -67,6 +67,20 @@ function handleParticles() {
     for (let i = 0; i < particlesArray.length; i++){
         particlesArray[i].update();
         particlesArray[i].draw();
+        
+        for (let j = 0; j < particlesArray.length; j++){
+            const dx = particlesArray[i].x - particlesArray[j].x;
+            const dy = particlesArray[i].y - particlesArray[j].y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+            if (distance < 100) {
+                ctx.beginPath();
+                ctx.strokeStyle = particlesArray[i].color;
+                ctx.lineWidth = 0.2;
+                ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
+                ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
+                ctx.stroke();
+            }
+        }
         if (particlesArray[i].size <= 0.3) {
             particlesArray.splice(i, 1);
             i--;
@@ -77,8 +91,7 @@ function handleParticles() {
 console.log(particlesArray)
 
 function animate() {
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.02)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
     handleParticles();
     hue += 0.5;
     requestAnimationFrame(animate);
